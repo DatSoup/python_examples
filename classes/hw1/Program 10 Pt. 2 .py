b09186ff5413911b5e120b9586dc024075b4222e
DatSoup <100242733+DatSoup@users.noreply.github.com>
2022-04-30 15:07:26 -0500

    Rename/corrections
    Renamed the files and made corrections following grading.

--- classes/hw1/Program 10 Pt. 2 .py
+++ classes/hw1/Program 10 Pt. 2 .py
@@ -1,37 +1,57 @@
-import os
+#Chris Campbell
+#c.campbell.1@und.edu
+#Program 10 part 2
 
 def readClasses (fileName):
     ClassDict = {}
-    inFile = open("./local_data/" + fileName, 'r')
+    inFile = open(fileName, 'r')
     for line in inFile:
         line = line.strip()
         key, value = line.split('\t')
         ClassDict[key] = int(value)
+        
     return ClassDict
-    
+
+
 def totalCredits (theDictionary):
     totalCredits = sum(theDictionary.values())
-    return print("Total number of credits this semester: ", totalCredits)
-
+    return totalCredits
+        
+        
+    
 def classesByDepartment(theDictionary, department):
-    num = 0
-    for item in theDictionary:
+    classByDept = []
+    for item in theDictionary.keys():
         if item.startswith(department):
-            num += 1
-    return print("The number of classes for this department is: ", num)
+            classByDept.append(item)
+    return classByDept
 
+ 
+def classByCredits (theDictionary, credits):
+    classByCredit = []
+    for k,v in theDictionary.items():
+        if v==credits:
+            classByCredit.append(k)
+    return classByCredit
+    
 def isFullTime(theDictionary):
-    return sum(theDictionary.values()) >= 12
-
+    if sum(theDictionary.values()) >= 12:
+        return True
+    else:
+        return False
+    
 def numOfClasses(theDictionary):
     return len(theDictionary)
-
+    
 def upperLevelCredits (theDictionary):
     creds = 0
     for k, v in theDictionary.items():
-        if (courses := k.rsplit(' ')) and (len(courses) >= 2 and int(courses[1]) >= 300):
+        courses = k.rsplit(' ')
+        if len(courses) >= 2 and int(courses[1]) >= 300: #len(courses) verifies that the class was typed correctly and a split occured between the class type and number
             creds+= v
-    return print("Number of upper level classes is: ", creds)
+    return creds 
+            
+        
 
 def printClasses(theDictionary):
     width = 8
@@ -39,15 +59,17 @@ def printClasses(theDictionary):
     print("-------------------------------")
     for k , v  in theDictionary.items():
         print("%-5s %15s" % (f'{k:<{width}}' , f'{v:>{width}}'))
+    print("Total number of credits this semester: ", sum(theDictionary.values()))
 
-def main():
-    dictionary_file = os.getenv('DICTIONARY') or 'ClassDictionary.txt'
-    ClassDict = readClasses(dictionary_file)
+    
+def main(): 
+    ClassDict = readClasses(input("Enter a text file: "))
     totalCredits (ClassDict)
     classesByDepartment(ClassDict, 'CSCI')
+    classByCredits(ClassDict, 4)
     isFullTime(ClassDict)
     numOfClasses(ClassDict)
     upperLevelCredits (ClassDict)
     printClasses(ClassDict)
-    print(ClassDict)
+    
 main()
